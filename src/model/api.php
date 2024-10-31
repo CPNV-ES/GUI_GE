3c38f2d51f3c19c976bcfe823000a0082cbd3c08
Ethann Schneider <ethann@shkermit.ch>
2024-10-31 18:03:30 +0100

    feat: Enhanced user interface with improved scrollbars and anime airing schedule display.

--- src/model/api.php
+++ src/model/api.php
@@ -2,7 +2,6 @@
 function getAnimeDetails($id) {
     $url = 'https://graphql.anilist.co';
 
-    // GraphQL query to get anime details by ID
     $query = '
         query ($id: Int) {
             Media(id: $id, type: ANIME) {
@@ -38,12 +37,10 @@ function getAnimeDetails($id) {
         }
     ';
 
-    // Variables for the GraphQL query
     $variables = [
         "id" => $id
     ];
 
-    // Initialize cURL
     $ch = curl_init();
 
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -58,14 +55,11 @@ function getAnimeDetails($id) {
         'variables' => $variables
     ]));
 
-    // Disable SSL verification (only for development)
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
-    // Execute the request
     $response = curl_exec($ch);
 
-    // Check for errors
     if(curl_errno($ch)) {
         echo 'Error:' . curl_error($ch);
         return null;
@@ -73,38 +67,28 @@ function getAnimeDetails($id) {
 
     curl_close($ch);
 
-    // Decode the JSON response
     $data = json_decode($response, true);
 
-    // Extract and format the details if available
     if (isset($data['data']['Media'])) {
         $media = $data['data']['Media'];
 
-        // Extract title
         $title = $media['title']['romaji'] ?? $media['title']['english'] ?? $media['title']['native'];
 
-        // Extract episodes count
         $totalEpisodes = $media['episodes'] ?? 'Unknown';
-        // Calculate aired episodes based on status and nextAiringEpisode
         $airedEpisodes = 0;
         if ($media['status'] === 'RELEASING' && isset($media['nextAiringEpisode']['episode'])) {
-            // If it's currently airing, calculate aired episodes
             $airedEpisodes = $media['nextAiringEpisode']['episode'] - 1;
         } elseif ($media['status'] === 'FINISHED') {
-            // If the anime has finished airing, all episodes are aired
             $airedEpisodes = $totalEpisodes;
         }
 
-        // Format start and end dates
         $startDate = isset($media['startDate']) ? formatDate($media['startDate']) : "Unknown";
         $endDate = isset($media['endDate']) ? formatDate($media['endDate']) : "Unknown";
 		
         $startDateN = isset($media['startDate']) ? formatDateN($media['startDate']) : null;
         $endDateN = isset($media['endDate']) ? formatDateN($media['endDate']) : null;
-        // Format season information
         $season = $media['season'] ? ucfirst(strtolower($media['season'])) . ' ' . $media['seasonYear'] : 'Unknown';
 
-        // Return the details as an associative array
         return [
             'title' => [
 				'romaji' => $media['title']['romaji'],
@@ -144,4 +128,127 @@ function formatDateN($date) {
         return $dateTime ? $dateTime->format('Y-m-d') : null;
     }
     return null;
+}
+
+
+function getAnimeAiringNext7Days() {
+    $airingSoon = [];
+	for ($i = 1; $i <= 3; $i++) {
+		$airingSoon = getAnimeAiringPage($i);
+	}
+
+    return $airingSoon;
+}
+
+function getAnimeAiringPage($page) {
+    $url = 'https://graphql.anilist.co';
+
+    $currentDate = time();
+    $next7Days = $currentDate + (7 * 24 * 60 * 60);
+
+    $query = '
+            query ($page: Int) {
+                Page(page: $page, perPage: 50) {
+                    media(type: ANIME, status: RELEASING, sort: POPULARITY_DESC) {
+						id
+                        title {
+                            romaji
+                            english
+                            native
+                        }
+                        nextAiringEpisode {
+                            episode
+                            airingAt
+                        }
+						coverImage {
+							medium
+						}
+                    }
+                    pageInfo {
+                        hasNextPage
+                    }
+                }
+            }
+        ';
+
+	$variables = [
+		"$page" => $page
+	];
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        'Accept: application/json'
+    ]);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+        'query' => $query,
+		'variables' => $variables
+    ]));
+
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+    $response = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+        return null;
+    }
+
+    curl_close($ch);
+
+    $data = json_decode($response, true);
+
+    $airingSoon = [];
+    if (isset($data['data']['Page']['media'])) {
+        foreach ($data['data']['Page']['media'] as $anime) {
+            if (isset($anime['nextAiringEpisode']['airingAt']) &&
+                $anime['nextAiringEpisode']['airingAt'] >= $currentDate &&
+                $anime['nextAiringEpisode']['airingAt'] <= $next7Days) {
+                $airingDate = date('Y-m-d H:i:s', $anime['nextAiringEpisode']['airingAt']);
+				$airingTime =  date('g:i A', $anime['nextAiringEpisode']['airingAt']);
+                $airingSoon[] = [
+					'title' => [
+					'romaji' => $anime['title']['romaji'],
+					'english' => $anime['title']['english'],
+					'native' => $anime['title']['native'],
+					],
+                    'episode' => $anime['nextAiringEpisode']['episode'],
+                    'airingAt' => $airingDate,
+					'time' => $airingTime,
+					'id' => $anime['id'],
+					'image' => $anime['coverImage']['medium']
+                ];
+            }
+        }
+    }
+
+    return $airingSoon;
+}
+function groupAnimeByAiringDate($animeList) {
+    $groupedAnime = [];
+
+    foreach ($animeList as $anime) {
+        $airingDate = date('l j M', strtotime($anime['airingAt']));
+        if (!isset($groupedAnime[$airingDate])) {
+            $groupedAnime[$airingDate] = [];
+        }
+        $groupedAnime[$airingDate][] = $anime;
+    }
+
+    return $groupedAnime;
+}
+
+function getNextSevenDays() {
+    $dates = [];
+    for ($i = 0; $i < 7; $i++) {
+        $date = date('l j M', strtotime("+$i days"));
+        $dates[] = $date;
+    }
+
+    return $dates;
 }
--- src/view/list_anime.php
+++ src/view/list_anime.php
@@ -1,79 +1,48 @@
 <?php
+require_once MODEL_DIR . "/api.php";
+$upcomingAnime = getAnimeAiringNext7Days();
+$upcomingAnime = groupAnimeByAiringDate($upcomingAnime);
+
 $title = 'Anime Airing Schedule';
 $style = "<link rel=\"stylesheet\" href=\"/assets/css/daylist.css\" />";
 ob_start();
 ?>
 <div class="container" id="container">
+    <?php
+$nextSevenDays = getNextSevenDays();
+foreach ($nextSevenDays as $date) {
+    ?>
     <div class="day-container">
-        <div class="day-title">Monday 07.10</div>
-
+        <div class="day-title"><?=$date?></div>
+        <?php
+            foreach (isset($upcomingAnime[$date]) ? $upcomingAnime[$date] : [] as $anime) {
+                ?>
         <div class="episode-card">
-            <a href="#" class="episode-link">
-                <div class="episode-image"></div>
+            <a href="/anime/<?= $anime['id'] ?>" class="episode-link">
+                <img
+                    src="<?=$anime['image']?>"
+                    alt=""
+                    class="episode-image">
                 <div class="episode-info">
-                    <div class="episode-title">Title</div>
-                    <div class="episode-time">Ep 11 aired at 06:00 AM</div>
+                    <div class="episode-title">
+                        <?=$anime['title']['romaji']?>
+                    </div>
+                    <div class="episode-time">Ep
+                        <?=$anime['episode']?>
+                        aired at
+                        <?=$anime['time']?>
+                    </div>
                 </div>
             </a>
-            <div class="add-button">+</div>
+            <div class="add-button"><a href="/edit_anime/<?= $anime['id'] ?>">+</a></div>
         </div>
+        <?php
+            }
+    ?>
     </div>
-    <div class="day-container">
-        <div class="day-title">Tuesday 07.11</div>
-
-        <div class="episode-card">
-            <a href="#" class="episode-link">
-                <div class="episode-image"></div>
-                <div class="episode-info">
-                    <div class="episode-title">Title</div>
-                    <div class="episode-time">Ep 11 aired at 06:00 AM</div>
-                </div>
-            </a>
-            <div class="add-button">+</div>
-        </div>
-    </div>
-    <div class="day-container">
-        <div class="day-title">Wednesday 07.11</div>
-
-        <div class="episode-card">
-            <a href="#" class="episode-link">
-                <div class="episode-image"></div>
-                <div class="episode-info">
-                    <div class="episode-title">Title</div>
-                    <div class="episode-time">Ep 11 aired at 06:00 AM</div>
-                </div>
-            </a>
-            <div class="add-button">+</div>
-        </div>
-    </div>
-    <div class="day-container">
-        <div class="day-title">Thurday 07.11</div>
-
-        <div class="episode-card">
-            <a href="#" class="episode-link">
-                <div class="episode-image"></div>
-                <div class="episode-info">
-                    <div class="episode-title">Title</div>
-                    <div class="episode-time">Ep 11 aired at 06:00 AM</div>
-                </div>
-            </a>
-            <div class="add-button">+</div>
-        </div>
-    </div>
-    <div class="day-container">
-        <div class="day-title">Friday 07.11</div>
-
-        <div class="episode-card">
-            <a href="#" class="episode-link">
-                <div class="episode-image"></div>
-                <div class="episode-info">
-                    <div class="episode-title">Title</div>
-                    <div class="episode-time">Ep 11 aired at 06:00 AM</div>
-                </div>
-            </a>
-            <div class="add-button">+</div>
-        </div>
-    </div>
+    <?php
+}
+?>
 </div>
 <?php
 $content = ob_get_clean();
